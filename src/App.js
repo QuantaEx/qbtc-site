8b224ae851ef8b53df30f67c6ce483929ca6b2ea
T. Rahaman <tanvirtex@gmail.com>
2020-08-14 09:56:16 +0600

    qbtc

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/*eslint-disable*/
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <div className="App-link">
+          Copyright 2018-2020 &#8211; WWW.QUANTAEX.COM | NEXBIT.IO LTD.
+         </div>
+       </div>
+  );
+}
+
+export default App;
